c297c411a55f75ce5ea8b32e2779001796ee8c6c
Jan Tourlamain <jan.tourlamain@outlook.com>
2017-05-16 15:18:15 +0200

    oldpage

--- src/DevProtocol.Xam.SwapiRui.UI/Pages/Old/OldPage.xaml.cs
+++ src/DevProtocol.Xam.SwapiRui.UI/Pages/Old/OldPage.xaml.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using DevProtocol.Xam.SwapiRui.Core.ViewModels;
+
 using Xamarin.Forms;
 
 namespace DevProtocol.Xam.SwapiRui.UI.Pages.Old
 {
-	public partial class OldPage : ContentPage
-	{
-		public OldPage()
-		{
-			InitializeComponent();
-			BindingContext = new OldViewModel();
-		}
-	}
+    public partial class OldPage : ContentPage
+    {
+        public OldPage()
+        {
+            InitializeComponent();
+        }
+    }
 }
